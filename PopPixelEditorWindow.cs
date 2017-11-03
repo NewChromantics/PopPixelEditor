a3cfc047acfb00b4e6001b94ce1f68b0b2ef4273
Graham Reeves <graham@newchromantics.com>
2017-11-03 15:42:27 +0000

    When no texture selected, a new-texture menu appears which can save to different formats

--- PopPixelEditorWindow.cs
+++ PopPixelEditorWindow.cs
@@ -32,6 +32,15 @@ namespace Pop
 		public float	ScrollY = 0;
 		bool			ModifyAlpha = false;
 
+
+
+		//	attributes for new texture
+		int				NewTexture_Width = 16;
+		int				NewTexture_Height = 16;
+		Color			NewTexture_Colour = Color.green;
+		Shader			NewTexture_InitShader = null;
+		Material		NewTexture_InitMaterial = null;
+
 		void Awake()
 		{
 			this.wantsMouseMove = true;
@@ -260,6 +269,98 @@ namespace Pop
 			*/
 		}
 
+
+		void OnNewTextureGUI()
+		{
+			EditorGUILayout.HelpBox ("Select a texture asset", MessageType.Error);
+
+			EditorGUILayout.HelpBox ("Create new texture asset (PNG)", MessageType.Info);
+			NewTexture_Width = EditorGUILayout.IntSlider ("Width", NewTexture_Width, 0, 4096, null);
+			NewTexture_Height = EditorGUILayout.IntSlider ("Height", NewTexture_Height, 0, 4096, null);
+			NewTexture_Colour = EditorGUILayout.ColorField("Colour", NewTexture_Colour );
+
+			//	show shader option when there's no material
+			if ( NewTexture_InitMaterial == null )
+				NewTexture_InitShader = EditorGUILayout.ObjectField ("Initialise with blit", NewTexture_InitShader, NewTexture_InitShader.GetType(), true, null) as Shader;
+			NewTexture_InitMaterial = EditorGUILayout.ObjectField ("Initialise with blit", NewTexture_InitMaterial, NewTexture_InitMaterial.GetType(), true, null) as Material;
+
+			System.Func<Texture2D,byte[]> EncodePng = (Texture) => {
+				return Texture.EncodeToPNG ();
+			};
+			System.Func<Texture2D,byte[]> EncodeExr = (Texture) => {
+				return Texture.EncodeToEXR ();
+			};
+			System.Func<Texture2D,byte[]> EncodeJpeg = (Texture) => {
+				return Texture.EncodeToJPG ();
+			};
+
+			System.Func<Texture2D,byte[]> EncodeFunc = null;
+			string EncodeExtension = null;
+
+			if (GUILayout.Button ("Save as .png...")) {
+				EncodeFunc = EncodePng;
+				EncodeExtension = "png";
+			}
+			else if (GUILayout.Button ("Save as .jpeg...")) {
+				EncodeFunc = EncodeJpeg;
+				EncodeExtension = "jpg";
+			}
+			else if (GUILayout.Button ("Save as .exr...")) {
+				EncodeFunc = EncodeExr;
+				EncodeExtension = "exr";
+			}
+
+			if ( EncodeFunc != null )
+			{
+				var NewRenderTexture = RenderTexture.GetTemporary (NewTexture_Width, NewTexture_Height, 0, RenderTextureFormat.ARGBFloat);
+				var FillTexture = new Texture2D (1, 1, TextureFormat.ARGB32, false);
+				FillTexture.SetPixel (0, 0, NewTexture_Colour);
+				FillTexture.Apply ();
+
+				Material BlitMaterial = null;
+				if (NewTexture_InitMaterial != null)
+					BlitMaterial = NewTexture_InitMaterial;
+				else if (NewTexture_InitShader)
+					BlitMaterial = new Material (NewTexture_InitShader);
+
+				if ( BlitMaterial != null )
+					Graphics.Blit (FillTexture, NewRenderTexture, BlitMaterial);
+				else
+					Graphics.Blit (FillTexture, NewRenderTexture);
+
+				//	gr: reuse the Pop SaveToPng functions!
+				var NewTexture = new Texture2D (NewTexture_Width, NewTexture_Height, TextureFormat.RGBAFloat, false);
+				RenderTexture.active = NewRenderTexture;
+				NewTexture.ReadPixels (new Rect (0, 0, NewRenderTexture.width, NewRenderTexture.height), 0, 0);
+
+				RenderTexture.active = null;
+				RenderTexture.ReleaseTemporary (NewRenderTexture);
+
+				byte[] Bytes = EncodeFunc(NewTexture);
+				string Filename = EditorUtility.SaveFilePanel("save new texture", "Assets", NewTexture_Width + "x" + NewTexture_Height, EncodeExtension);
+				if (Filename.Length == 0)
+					throw new System.Exception ("User cancelled save");
+				
+				System.IO.File.WriteAllBytes( Filename, Bytes );
+
+				//	make new file show up
+				AssetDatabase.Refresh();
+
+				//	try and select new asset 
+				var AssetPath = Filename;
+				if (AssetPath.StartsWith (Application.dataPath)) {
+					AssetPath = AssetPath.Remove (0, Application.dataPath.Length);
+					AssetPath = "Assets" + AssetPath;
+					var NewAsset = AssetDatabase.LoadAssetAtPath<Texture2D> (AssetPath);
+					if (NewAsset != null) {
+						//	gr: unity by default imports at power of 2... see if we can fix that
+						UnityEditor.Selection.activeObject = NewAsset;
+					}
+				}
+			}
+
+		}
+
 		void OnGUI()
 		{
 			var MouseEvent = Event.current;
@@ -272,7 +373,7 @@ namespace Pop
 			if (EditingTexture != null) {
 				OnTextureGui (EditingTexture);
 			} else {
-				EditorGUILayout.HelpBox ("Select a texture asset", MessageType.Error);
+				OnNewTextureGUI ();
 			}
 
 		}
